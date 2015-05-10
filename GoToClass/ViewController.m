af2052c4feecd542340bfc856185ff4c1682ab58
Li Zhenliang <coldlefthand@gmail.com>
2015-05-10 19:10:33 +0800

    Facebook pop animation

--- GoToClass/ViewController.m
+++ GoToClass/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "TeacherListVC.h"
+#import <pop/POP.h>
 
 @interface ViewController ()
 
@@ -16,9 +18,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    UINavigationController *vc =  self.navigationController;
-    vc.navigationBarHidden = YES;
-    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -27,4 +26,23 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)btnSearchTeacher:(id)sender {
+    
+    POPSpringAnimation *springAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPViewCenter];
+    springAnimation.fromValue = [NSValue valueWithCGPoint:CGPointMake(self.view.bounds.size.width/2, -self.view.bounds.size.height/2)];
+
+    springAnimation.toValue = [NSValue valueWithCGPoint:CGPointMake(self.view.bounds.size.width/2, self.view.bounds.size.height/2)];
+    
+    
+    //弹性值
+    springAnimation.springBounciness = 20.0;
+    //弹性速度
+    springAnimation.springSpeed = 10.0;
+    
+    UINavigationController *vc =  [self.storyboard instantiateViewControllerWithIdentifier:@"TeachersListNC"];
+    
+    [vc.view pop_addAnimation:springAnimation forKey:@"WhatEverNameYouWant"];
+    
+    [self presentViewController:vc animated:NO completion:nil];
+}
 @end
